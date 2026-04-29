f4c646bfe1e56c64da5deb79166c2f6615c20286
Rohit Anand <25rohitanand@gmail.com>
2026-04-29 13:34:06 +0530

    fix:disables nodemailer(render timeout issue)

--- backend/index.js
+++ backend/index.js
@@ -32,13 +32,13 @@ app.post("/api/contact", async (req, res) => {
     await newMessage.save();
 
       // 2. Send Email
-      const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-      });
+      // const transporter = nodemailer.createTransport({
+      // service: "gmail",
+      // auth: {
+      //   user: process.env.EMAIL_USER,
+      //   pass: process.env.EMAIL_PASS,
+      // },
+      // });
 
       // await transporter.sendMail({
       // from: email,
